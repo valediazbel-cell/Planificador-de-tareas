79181181d5a1a23abbee02cde133a8f2bc7b58ff
ValeriaDiaz <valeriadiazbel@gmail.com>
2026-08-28 18:07:07 -0500

    tarea nuevas

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,77 @@
 
+
+
 const taskManager = new TaskManager();
+
+/*taskManager.addTask(
+ 'Sacar la basura',
+ 'Sacar la basura al frente de la casa',
+ '2020-09-20',
+  'Alta',
+ 'PORHACER'
+);
 console.log(taskManager.tasks);
+
+taskManager.addTask(
+ 'Hacer de cenar',
+ 'Preparar la cena para la familia',
+ '2020-09-20',
+ 'Alta',
+ 'PORHACER'
+);
+console.log(taskManager.tasks);*/
+
+
+const form = document.getElementById("newTaskForm");
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+console.log ("entramos");
+validFormFieldInput();
+if ( validFormFieldInput() == true ){
+console.log ("todo lleno");
+    const nombre = validationTarea.value.trim();
+    const descricion = validationDes.value.trim();
+    const fecha = validacionfecha.value;
+    const prioridad = validationprio.value;
+    const status = "PORHACER";
+    taskManager.addTask(
+        nombre,
+        descricion,
+        fecha,
+        prioridad,
+        status
+    );
+   console.log(taskManager.tasks);
+   pintarTarea();
+   //form.reset();
+} 
+
+});
+
+
+
+function pintarTarea(){
+let listaTareas = document.getElementById("lista-tareas");
+
+for (let k = 0; k< taskManager.tasks.length; k++){
+    let nuevaTarea = document.createElement("li");
+    nuevaTarea.classList.add ("list-grup-item","tarea");
+
+    nuevaTarea.innerHTML = '<div>'+
+                        '<input class="form-check-input me-1" type="checkbox" value="" id="firstCheckbox">'+
+                        '<label class="form-check-label" for="firstCheckbox">'+ taskManager.tasks[k].name+'</label>'+
+                          ' </div>'+
+                   '<div>'+
+                      '  <button type="button" class="btn btn-outline-primary botonEliminar">Eliminar</button>'+
+                    '</div>'
+
+                    listaTareas.appendChild(nuevaTarea);
+                 
+}
+
+
+}
 
 
 
@@ -19,7 +90,7 @@ let alertPrio = document.getElementById("alertPrio")
 
 
 
-function validFormFieldInput(data) {
+function validFormFieldInput() {
     const nombre = validationTarea.value;
     const descricion = validationDes.value;
     const fecha = validacionfecha.value;
@@ -31,6 +102,7 @@ function validFormFieldInput(data) {
         alertTarea.innerHTML = "Ingresa el nombre de la tarea"
         alertTarea.classList.remove("ocultaAlert");
         console.log("esta vacio, coloca la tarea")
+        return false
     } else {
         alertTarea.classList.add("ocultaAlert");
     }
@@ -38,6 +110,7 @@ function validFormFieldInput(data) {
         alertDes.innerHTML = "La descripción es obligatoria"
         alertDes.classList.remove("ocultaAlert");
         console.log("esta vacio,coloca la descripcion ")
+         return false
     } else {
         alertDes.classList.add("ocultaAlert");
     }
@@ -45,6 +118,7 @@ function validFormFieldInput(data) {
         alertFecha.innerHTML = "Selecciona una fecha"
         alertFecha.classList.remove("ocultaAlert");
         console.log("esta vacio,coloca la fecha")
+        return false
     }else {
         alertFecha.classList.add("ocultaAlert");
     }
@@ -52,19 +126,18 @@ function validFormFieldInput(data) {
         alertPrio.innerHTML = "Selecciona una prioridad"
         alertPrio.classList.remove("ocultaAlert");
         console.log("esta vacio, coloca la prioridad")
+         return false
     }else {
         alertPrio.classList.add("ocultaAlert");
     }
+    return true
+
+} 
 
 
-}
 
-const checkboxes =document.querySelectorAll(".form-check-input")
 
-checkboxes.forEach(function(checkbox){
-    checkbox.addEventListener("change",function(){
-        const tarea = checkbox.closest(".tarea");
-tarea.classList.toggle("tarea-completada")
-    });
-});
+
+
+
 
--- JS/taskManager.js
+++ JS/taskManager.js
@@ -1,6 +1,23 @@
 
 class TaskManager {
- constructor() {
+ constructor(currentId = 0) {
  this.tasks = [];
+ this.currentId = currentId;
  }
+
+ addTask(name,description,dueDate,priorida,status){
+    this.currentId ++;
+
+    this.tasks.push({
+        id:this.currentId,
+        name:name,
+        description:description,
+        dueDate:dueDate,
+        priorida:priorida,
+        status:'PORHACER'
+    });
+ }
+
+ 
 }
+
