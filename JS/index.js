863a777c4f0af872c944e981fc2d6e9ec8423ac8
ValeriaDiaz <valeriadiazbel@gmail.com>
2026-09-05 13:49:04 -0500

    arreglos footer y nombre

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,66 @@
+let validationTarea = document.getElementById("validationTarea");
+let validationDes = document.getElementById("validationDes");
+let validacionfecha = document.getElementById("validacionfecha");
+let validationprio = document.getElementById("validationprio");
+
+
+
+
+
+//ALERTAS
+let alertTarea = document.getElementById("alertTarea")
+let alertDes = document.getElementById("alertDes")
+let alertFecha = document.getElementById("alertFecha")
+let alertPrio = document.getElementById("alertPrio")
+
+
+
+
+function validFormFieldInput() {
+    const nombre = validationTarea.value;
+    const descricion = validationDes.value;
+    const fecha = validacionfecha.value;
+    const prioridad = validationprio.value;
+
+
+
+    if (nombre.trim() === "") {
+        alertTarea.innerHTML = "Ingresa el nombre de la tarea"
+        alertTarea.classList.remove("ocultaAlert");
+        console.log("esta vacio, coloca la tarea")
+        return false
+    } else {
+        alertTarea.classList.add("ocultaAlert");
+    }
+    if (descricion.trim() === "") {
+        alertDes.innerHTML = "La descripción es obligatoria"
+        alertDes.classList.remove("ocultaAlert");
+        console.log("esta vacio,coloca la descripcion ")
+        return false
+    } else {
+        alertDes.classList.add("ocultaAlert");
+    }
+    if (fecha.trim() === "") {
+        alertFecha.innerHTML = "Selecciona una fecha"
+        alertFecha.classList.remove("ocultaAlert");
+        console.log("esta vacio,coloca la fecha")
+        return false
+    } else {
+        alertFecha.classList.add("ocultaAlert");
+    }
+    if (prioridad.trim() === "") {
+        alertPrio.innerHTML = "Selecciona una prioridad"
+        alertPrio.classList.remove("ocultaAlert");
+        console.log("esta vacio, coloca la prioridad")
+        return false
+    } else {
+        alertPrio.classList.add("ocultaAlert");
+    }
+    return true
+
+}
+
+
 let taskManager;
 
 const tareasGuardadas = localStorage.getItem("tarea");
@@ -149,67 +212,6 @@ pintarTarea();
 
 
 
-let validationTarea = document.getElementById("validationTarea");
-let validationDes = document.getElementById("validationDes");
-let validacionfecha = document.getElementById("validacionfecha");
-let validationprio = document.getElementById("validationprio");
-
-
-
-
-
-//ALERTAS
-let alertTarea = document.getElementById("alertTarea")
-let alertDes = document.getElementById("alertDes")
-let alertFecha = document.getElementById("alertFecha")
-let alertPrio = document.getElementById("alertPrio")
-
-
-
-
-function validFormFieldInput() {
-    const nombre = validationTarea.value;
-    const descricion = validationDes.value;
-    const fecha = validacionfecha.value;
-    const prioridad = validationprio.value;
-
-
-
-    if (nombre.trim() === "") {
-        alertTarea.innerHTML = "Ingresa el nombre de la tarea"
-        alertTarea.classList.remove("ocultaAlert");
-        console.log("esta vacio, coloca la tarea")
-        return false
-    } else {
-        alertTarea.classList.add("ocultaAlert");
-    }
-    if (descricion.trim() === "") {
-        alertDes.innerHTML = "La descripción es obligatoria"
-        alertDes.classList.remove("ocultaAlert");
-        console.log("esta vacio,coloca la descripcion ")
-        return false
-    } else {
-        alertDes.classList.add("ocultaAlert");
-    }
-    if (fecha.trim() === "") {
-        alertFecha.innerHTML = "Selecciona una fecha"
-        alertFecha.classList.remove("ocultaAlert");
-        console.log("esta vacio,coloca la fecha")
-        return false
-    } else {
-        alertFecha.classList.add("ocultaAlert");
-    }
-    if (prioridad.trim() === "") {
-        alertPrio.innerHTML = "Selecciona una prioridad"
-        alertPrio.classList.remove("ocultaAlert");
-        console.log("esta vacio, coloca la prioridad")
-        return false
-    } else {
-        alertPrio.classList.add("ocultaAlert");
-    }
-    return true
-
-}
 
 
 
